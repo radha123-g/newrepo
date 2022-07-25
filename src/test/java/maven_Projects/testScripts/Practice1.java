dcc9e00b5979e0de3ed914a9d508541d8a4c9d50
radha123-g <radhagaddennavar789@gmail.com>
2022-07-25 13:14:23 +0530

    firts commit

---
+++ src/test/java/maven_Projects/testScripts/Practice1.java
@@ -0,0 +1,22 @@
+package maven_Projects.testScripts;
+
+import org.testng.annotations.Test;
+public class Practice1 {
+	
+	
+@Test
+public void practice2() {
+	String BROWSER=System.getProperty("browser");
+	String URL=System.getProperty("url");
+	
+	
+	System.out.println("Test1--> Test1");
+}
+
+@Test
+public void practice3() {
+	System.out.println("Test3--> Test3");
+}
+
+
+}
---
+++ src/test/java/maven_Projects/testScripts/Practice2Test.java
@@ -0,0 +1,11 @@
+package maven_Projects.testScripts;
+
+import org.testng.annotations.Test;
+
+public class Practice2Test {
+@Test
+public void practice2Test() {
+	
+}
+
+}
